5d79f5511cbd6c32bce9171e5a14c40ddf7f9499
Adam Buckley <adam@2pisoftware.com>
2018-07-11 15:44:02 +1000

    Initial commit

---
+++ main.go
@@ -0,0 +1,40 @@
+// A fair chunk of code snippets are from https://golang.org/doc/articles/wiki/
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func main() {
+	// http.HandleFunc("/view/", makeHandler(viewHandler))
+	// http.HandleFunc("/edit/", makeHandler(editHandler))
+	// http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	// Try and get port from environment, set to 3000 as default
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":3000"
+	}
+
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, err := template.ParseFiles(tmpl + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
