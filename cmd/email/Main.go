19f3498f473d3279a358777bb5ad77f13f8ce786
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2018-10-19 15:29:50 +1100

    Added in config loading for Email struct

--- cmd/email/Main.go
+++ cmd/email/Main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/FetchWeb/Fetch/pkg/email"
+import (
+	"fmt"
+	"github.com/FetchWeb/Fetch/pkg/email"
+)
 
 func main() {
+	e, err := email.LoadFromConfig("../../configs/TestEmailConfig.json")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(e)
+
 	var mailService email.MailService
-	mailService.SendEmail()
+	err = mailService.SendEmail(e, []string{"taliesinwrmillhouse@gmail.com"}, "Hello world!")
+	if err != nil {
+		panic(err)
+	}
 }
