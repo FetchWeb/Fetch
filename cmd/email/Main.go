52a09d717bb1349a90d81560912a10f769007ae8
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2018-10-21 15:22:33 +1100

    Implemented new email system

--- cmd/email/Main.go
+++ cmd/email/Main.go
@@ -1,25 +1,46 @@
 package main
 
 import (
-	"io/ioutil"
+	"log"
+	"net/mail"
+	"net/smtp"
 
 	"github.com/FetchWeb/Fetch/pkg/email"
 )
 
 func main() {
-	e, err := email.LoadFromConfig("../../configs/TestEmailConfig.json")
-	if err != nil {
-		panic(err)
+	// e, err := email.LoadFromConfig("../../configs/TestEmailConfig.json")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// // messageBuf, err := ioutil.ReadFile("../../test/email/TestEmailTemplate.html")
+	// // if err != nil {
+	// // 	panic(err)
+	// // }
+
+	// var service email.Service
+	// err = service.SendEmail(e, []string{"taliesinwrmillhouse@gmail.com"}, string("Hello world!"))
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// compose the message
+	m := email.NewMessage("Hi", "this is the body")
+	m.From = mail.Address{Name: "From", Address: "from@example.com"}
+	m.To = []string{"taliesinwrmillhouse@gmail.com"}
+
+	// add attachments
+	if err := m.Attach("../../test/email/attachment.jpg", true); err != nil {
+		log.Fatal(err)
 	}
 
-	messageBuf, err := ioutil.ReadFile("../../test/email/TestEmailTemplate.html")
-	if err != nil {
-		panic(err)
-	}
+	// add headers
+	m.AddHeader("X-CUSTOMER-id", "xxxxx")
 
-	var service email.Service
-	err = service.SendEmail(e, []string{"taliesinwrmillhouse@gmail.com"}, string(messageBuf))
-	if err != nil {
-		panic(err)
+	// send it
+	auth := smtp.PlainAuth("", "fetchweb.test@gmail.com", "iumhivtjafwsxhhe", "smtp.gmail.com")
+	if err := email.Send("smtp.gmail.com:587", auth, m); err != nil {
+		log.Fatal(err)
 	}
 }
