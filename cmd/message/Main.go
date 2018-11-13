23c63772b278eaa76c5eecca2ec84a87a7e4993e
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2018-11-13 17:45:50 +1100

    Added comments to main.go

--- cmd/message/Main.go
+++ cmd/message/Main.go
@@ -8,12 +8,16 @@ import (
 )
 
 func main() {
+	// Startup the message service.
 	var s message.Service
 	if err := s.Startup(); err != nil {
 		panic(err)
 	}
 
+	// Start checking for queued emails.
 	go s.Check()
+
+	// Start listening at endpoint for incoming emails.
 	http.HandleFunc("/listen", s.Listen)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
