514fc4e1463f2a16e8899fdf7242b8834e71b51a
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2019-01-02 11:55:57 +1100

    Moved model files over to separate repo.

--- pkg/message/Service.go
+++ pkg/message/Service.go
@@ -1,108 +1,96 @@
 package message
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-	"net/smtp"
-	"strings"
-	"time"
+// // Service handles emails and other simlar functionality within the package.
+// type Service struct {
+// 	client     *redis.Client
+// 	clientKeys core.Queue
+// }
 
-	"github.com/FetchWeb/Fetch/pkg/core"
-	"github.com/go-redis/redis"
-)
+// func (s *Service) Startup() error {
+// 	s.client = redis.NewClient(&redis.Options{
+// 		Addr:     "localhost:6379",
+// 		Password: "",
+// 		DB:       0,
+// 	})
 
-// Service handles emails and other simlar functionality within the package.
-type Service struct {
-	client     *redis.Client
-	clientKeys core.Queue
-}
+// 	_, err := s.client.Ping().Result()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
-func (s *Service) Startup() error {
-	s.client = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
+// func (s *Service) Listen(w http.ResponseWriter, r *http.Request) {
+// 	email := &Email{}
+// 	err := json.NewDecoder(r.Body).Decode(email)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 		log.Print(err)
+// 		return
+// 	}
 
-	_, err := s.client.Ping().Result()
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// 	err = s.EnqueueEmail(email)
+// 	if err != nil {
+// 		log.Print(err)
+// 	}
+// }
 
-func (s *Service) Listen(w http.ResponseWriter, r *http.Request) {
-	email := &Email{}
-	err := json.NewDecoder(r.Body).Decode(email)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		log.Print(err)
-		return
-	}
+// func (s *Service) Check() {
+// 	for {
+// 		if !s.clientKeys.CanPop() {
+// 			time.Sleep(1000 * time.Millisecond)
+// 			continue
+// 		}
+// 		if err := s.DequeueEmail(); err != nil {
+// 			log.Print(err) // TODO: Add recovery functionality for email.
+// 		}
+// 	}
+// }
 
-	err = s.EnqueueEmail(email)
-	if err != nil {
-		log.Print(err)
-	}
-}
+// func (s *Service) EnqueueEmail(e *Email) error {
+// 	data, err := e.MarshalBinary()
+// 	if err != nil {
+// 		return err
+// 	}
 
-func (s *Service) Check() {
-	for {
-		if !s.clientKeys.CanPop() {
-			time.Sleep(1000 * time.Millisecond)
-			continue
-		}
-		if err := s.DequeueEmail(); err != nil {
-			log.Print(err) // TODO: Add recovery functionality for email.
-		}
-	}
-}
+// 	key := core.UniqueID()
+// 	s.clientKeys.Push(key)
 
-func (s *Service) EnqueueEmail(e *Email) error {
-	data, err := e.MarshalBinary()
-	if err != nil {
-		return err
-	}
+// 	err = s.client.Set(key, data, 0).Err()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
-	key := core.UniqueID()
-	s.clientKeys.Push(key)
+// func (s *Service) DequeueEmail() error {
+// 	key := s.clientKeys.Pop().(string)
 
-	err = s.client.Set(key, data, 0).Err()
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// 	data, err := s.client.Get(key).Result()
+// 	if err != nil {
+// 		return err
+// 	}
 
-func (s *Service) DequeueEmail() error {
-	key := s.clientKeys.Pop().(string)
+// 	var email Email
+// 	err = email.UnmarshalBinary([]byte(data))
+// 	if err != nil {
+// 		return err
+// 	}
 
-	data, err := s.client.Get(key).Result()
-	if err != nil {
-		return err
-	}
+// 	addr := strings.Join([]string{email.Credentials.Hostname, ":", email.Credentials.Port}, "")
+// 	auth := smtp.PlainAuth("", email.Credentials.Address, email.Credentials.Password, email.Credentials.Hostname)
 
-	var email Email
-	err = email.UnmarshalBinary([]byte(data))
-	if err != nil {
-		return err
-	}
+// 	err = smtp.SendMail(addr, auth, email.Credentials.Address, email.Data.GetRecipients(), email.Data.Data())
+// 	if err != nil {
+// 		return err
+// 	}
 
-	addr := strings.Join([]string{email.Credentials.Hostname, ":", email.Credentials.Port}, "")
-	auth := smtp.PlainAuth("", email.Credentials.Address, email.Credentials.Password, email.Credentials.Hostname)
-
-	err = smtp.SendMail(addr, auth, email.Credentials.Address, email.Data.GetRecipients(), email.Data.Data())
-	if err != nil {
-		return err
-	}
-
-	_, err = s.client.Del(key).Result()
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// 	_, err = s.client.Del(key).Result()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
 // // SendEmail sends an email with the message to the recipients from the sender.
 // func (s *Service) SendEmail(ec *EmailCredentials, ed *EmailData) error {
