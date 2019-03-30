0e852418bfc1520d487728a08ee34b2acd9502ec
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2019-03-30 22:20:57 +1100

    Added format variations to logging.

--- pkg/log/Log.go
+++ pkg/log/Log.go
@@ -2,7 +2,7 @@ package log
 
 import (
 	"encoding/json"
-	"log"
+	"fmt"
 	"os"
 	"time"
 
@@ -16,47 +16,77 @@ var logOptions *Options
 var logFile *os.File
 
 // Debug for logging any messages useful for debugging applications.
-func Debug(message string) error {
+func Debug(message string) {
 	if logOptions.LogDebug {
-		return writeLog("DEBUG", message)
+		writeLog("DEBUG", message)
 	}
-	return nil
+}
+
+// Debugf for logging any messages useful for debugging applications.
+func Debugf(message string, v ...interface{}) {
+	if logOptions.LogDebug {
+		writeLog("DEBUG", fmt.Sprintf(message, v...))
+	}
 }
 
 // Info for logging any messages about useful information.
-func Info(message string) error {
+func Info(message string) {
 	if logOptions.LogInfo {
-		return writeLog("INFO", message)
+		writeLog("INFO", message)
 	}
-	return nil
+}
+
+// Infof for logging any messages about useful information.
+func Infof(message string, v ...interface{}) {
+	if logOptions.LogInfo {
+		writeLog("INFO", fmt.Sprintf(message, v...))
+	}
 }
 
 // Warning for logging any warning messages. Example, bad practices.
-func Warning(message string) error {
+func Warning(message string) {
 	if logOptions.LogWarning {
-		return writeLog("WARNING", message)
+		writeLog("WARNING", message)
 	}
-	return nil
+}
+
+// Warningf for logging any warning messages. Example, bad practices.
+func Warningf(message string, v ...interface{}) {
+	if logOptions.LogWarning {
+		writeLog("WARNING", fmt.Sprintf(message, v...))
+	}
 }
 
 // Error for logging any error messages where the application is able to recover from.
-func Error(message string) error {
+func Error(message string) {
 	if logOptions.LogError {
-		return writeLog("ERROR", message)
+		writeLog("ERROR", message)
 	}
-	return nil
+}
+
+// Errorf for logging any error messages where the application is able to recover from.
+func Errorf(message string, v ...interface{}) {
+	if logOptions.LogError {
+		writeLog("ERROR", fmt.Sprintf(message, v...))
+	}
 }
 
 // Fatal for logging any fatal messages where the application is unable to recover from.
-func Fatal(message string) error {
+func Fatal(message string) {
 	if logOptions.LogFatal {
-		return writeLog("FATAL", message)
+		writeLog("FATAL", message)
 	}
-	return nil
+}
+
+// Fatalf for logging any fatal messages where the application is unable to recover from.
+func Fatalf(message string, v ...interface{}) {
+	if logOptions.LogFatal {
+		writeLog("FATAL", fmt.Sprintf(message, v...))
+	}
 }
 
 // Startup initialises the log package.
-func Startup(options *Options) {
+func Startup(options *Options) error {
 	if options == nil {
 		options = DefaultOptions()
 	}
@@ -66,7 +96,7 @@ func Startup(options *Options) {
 	var err error
 	logFile, err = os.OpenFile(fetch.JoinStrings(logOptions.Directory, time.Now().Format("Log_2006-01-02.json")), os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
-		log.Printf("Error opening log file: %v", err)
+		return err
 	}
 
 	output, err := json.Marshal(&Data{
@@ -77,10 +107,11 @@ func Startup(options *Options) {
 	})
 
 	if err != nil {
-		log.Printf("Error marshaling JSON: %v", err)
+		return err
 	}
 
 	logFile.Write(append([]byte("["), output...))
+	return nil
 }
 
 // Shutdown shuts down the log package.
@@ -90,19 +121,13 @@ func Shutdown() {
 }
 
 // writeLog writes the log message to console and file depending on which configuration is checked.
-func writeLog(level string, message string) error {
-	output, err := json.Marshal(&Data{
+func writeLog(level string, message string) {
+	output, _ := json.Marshal(&Data{
 		Level:     level,
 		Prefix:    logOptions.Prefix,
 		Timestamp: time.Now().Format("2006-01-02 15:04:05"),
 		Message:   message,
 	})
 
-	if err != nil {
-		return err
-	}
-
 	logFile.Write(append([]byte(","), output...))
-
-	return nil
 }
--- test/log/Log_test.go
+++ test/log/Log_test.go
@@ -1,6 +1,7 @@
 package log_test
 
 import (
+	"errors"
 	"os"
 	"testing"
 	"time"
@@ -20,25 +21,17 @@ func TestLogging(t *testing.T) {
 		Directory:  "logs/",
 	})
 
-	if err := log.Info("This is a test info message"); err != nil {
-		t.Errorf("Failed to log Info message: %v", err)
-	}
-
-	if err := log.Debug("This is a test debug message"); err != nil {
-		t.Errorf("Failed to log Debug message: %v", err)
-	}
-
-	if err := log.Warning("This is a test warning message"); err != nil {
-		t.Errorf("Failed to log Warning message: %v", err)
-	}
-
-	if err := log.Error("This is a test error message"); err != nil {
-		t.Errorf("Failed to log Error message: %v", err)
-	}
-
-	if err := log.Fatal("This is a test fatal message"); err != nil {
-		t.Errorf("Failed to log Fatal message: %v", err)
-	}
+	log.Info("This is a test info message")
+	log.Debug("This is a test debug message")
+	log.Warning("This is a test warning message")
+	log.Error("This is a test error message")
+	log.Fatal("This is a test fatal message")
+	
+	log.Infof("This is a test info %v", errors.New("message"))
+	log.Debugf("This is a test debug %v", errors.New("message"))
+	log.Warningf("This is a test warning %v", errors.New("message"))
+	log.Errorf("This is a test error %v", errors.New("message"))
+	log.Fatalf("This is a test fatal %v", errors.New("message"))
 
 	log.Shutdown()
 
@@ -59,28 +52,20 @@ func BenchmarkLogging(b *testing.B) {
 	})
 
 	for i := 0; i < b.N; i++ {
-		if err := log.Info("This is a test info message"); err != nil {
-			b.Errorf("Failed to log Info message: %v", err)
-		}
+		log.Info("This is a test info message")
+		log.Debug("This is a test debug message")
+		log.Warning("This is a test warning message")
+		log.Error("This is a test error message")
+		log.Fatal("This is a test fatal message")
 
-		if err := log.Debug("This is a test debug message"); err != nil {
-			b.Errorf("Failed to log Debug message: %v", err)
-		}
-
-		if err := log.Warning("This is a test warning message"); err != nil {
-			b.Errorf("Failed to log Warning message: %v", err)
-		}
-
-		if err := log.Error("This is a test error message"); err != nil {
-			b.Errorf("Failed to log Error message: %v", err)
-		}
-
-		if err := log.Fatal("This is a test fatal message"); err != nil {
-			b.Errorf("Failed to log Fatal message: %v", err)
-		}
-
-		log.Shutdown()
+		log.Infof("This is a test info %v", errors.New("message"))
+		log.Debugf("This is a test debug %v", errors.New("message"))
+		log.Warningf("This is a test warning %v", errors.New("message"))
+		log.Errorf("This is a test error %v", errors.New("message"))
+		log.Fatalf("This is a test fatal %v", errors.New("message"))
 	}
+	
+	log.Shutdown()
 
 	if err := os.Remove(fetch.JoinStrings("logs/", time.Now().Format("Log_2006-01-02.json"))); err != nil {
 		b.Logf("Failed to delete log file after test: %v", err)
